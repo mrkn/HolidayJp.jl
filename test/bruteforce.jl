bcdf929ed78052794058491fb9ac0ea6844596a5
Kenta Murata <mrkn@mrkn.jp>
2024-08-23 22:42:01 +0900

    test: Fix for Julia 1.6

--- test/bruteforce.jl
+++ test/bruteforce.jl
@@ -3,12 +3,13 @@ module BruteForceTest
 using Test
 
 import HolidayJp: HolidayJp, YAML
+import Dates: Day
 
 data_dir = joinpath(dirname(@__DIR__), "data")
 data = YAML.load_file(joinpath(data_dir, "holidays.yml"))
 
 date_begin, date_end = extrema(keys(data))
-for d in date_begin:date_end
+for d in date_begin:Day(1):date_end
     h = haskey(data, d)
     @testset "$(d) is$(h ? "" : " not") a holiday" begin
          @test HolidayJp.isholiday(d) === h
